7ee9e54abfb175cdc04863162fdf1c69af884134
Jimmy Givans <jauyoma.dev@gmail.com>
2023-09-16 18:36:52 +0300

    Echo standard input to standard output

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char const* argv[argc + 1])
 {
     (void) argc;
     (void) argv;
+
+    char buffer[512] = { '\0' };
+
+    while (!feof(stdin)) {
+        fgets(buffer, sizeof(buffer), stdin);
+        fputs(buffer, stdout);
+    }
 
     return EXIT_SUCCESS;
 }
