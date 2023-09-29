902896a6b265d47f85d582e71653949ac3fa4bb7
Jimmy Givans <jauyoma.dev@gmail.com>
2023-09-29 15:11:29 +0300

    Remove unused includes and rearrange the order of includes

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-
 #include "tee.h"
+
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const* argv[argc + 1])
 {
