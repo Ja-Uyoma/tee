b741ae63671204db982cd92ef5b4bb69937e9415
Jimmy Givans <jauyoma.dev@gmail.com>
2023-09-30 18:27:58 +0300

    Use getopt_long to handle command-line argument --help

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <getopt.h>
 
 int main(int argc, char* const argv[argc + 1])
 {
@@ -15,8 +17,37 @@ int main(int argc, char* const argv[argc + 1])
         }
     }
     else if (argc > 1) {
-        if (strcmp(argv[1], "--help") == 0) {
-            printHelp();
+        static struct option const longOptions[] = {
+            { .name = "help", .has_arg = no_argument, .flag = NULL, .val = 1 },
+            { 0, 0, 0, 0 }
+        };
+
+        int option = 0;
+        int optionIndex = 0;
+
+        while ((option = getopt_long(argc, argv, "", longOptions, &optionIndex)) != -1) {
+            switch (option) {
+            case 0:
+                break;
+
+            case 1:
+                printHelp();
+                break;
+
+            default:
+                break;
+            }
+
+            // Print any remaining command-line arguments (not options)
+            if (optind < argc) {
+                printf("\n\n%s", "Non-option argv elements: ");
+
+                while (optind < argc) {
+                    printf("%s", argv[optind++]);
+                }
+
+                puts("");
+            }
         }
     }
 
