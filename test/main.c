0bdee1aa1f1066fc240144028cfffe0ba1de91b2
Jimmy Givans <jauyoma.dev@gmail.com>
2023-10-05 17:18:28 +0300

    Add and run first unit test
    This is a null test. Its sole purpose is to confirm that everything is working
    as intended.

--- test/main.c
+++ test/main.c
@@ -4,7 +4,17 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+// A test case that does nothing and succeeds
+static void null_test_success(void** state)
+{
+    (void) state;
+}
+
 int main(void)
 {
-
+    struct CMUnitTest const tests[] = {
+        cmocka_unit_test(null_test_success)
+    };
+    
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
