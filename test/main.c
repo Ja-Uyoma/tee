bcb2f26596fad7d0a557f4757c42d1b603822353
Jimmy Givans <jauyoma.dev@gmail.com>
2023-10-05 21:26:37 +0300

    Add test case for the function echoToStdout
    - This test executes the function echoToStdout and checks if its return value is
    equal to 0 or not.

--- test/main.c
+++ test/main.c
@@ -4,16 +4,30 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+#include "tee.h"
+#include <errno.h>
+
 // A test case that does nothing and succeeds
 static void null_test_success(void** state)
 {
     (void) state;
 }
 
+static void echoToStdoutTest(void** state)
+{
+    (void) state;
+
+    char buffer[256] = { '\0' };
+    errno = 0;
+    
+    assert_return_code(echoToStdout(buffer, sizeof buffer), errno);
+}
+
 int main(void)
 {
     struct CMUnitTest const tests[] = {
-        cmocka_unit_test(null_test_success)
+        cmocka_unit_test(null_test_success),
+        cmocka_unit_test(echoToStdoutTest)
     };
     
     return cmocka_run_group_tests(tests, NULL, NULL);
