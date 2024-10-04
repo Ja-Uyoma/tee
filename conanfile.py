c737aa5d12f24f24a80693c4237324804ac4971d
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-04 11:53:59 +0300

    build: make Conan to follow the structure of a CMake project

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
 
 class TeeRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -6,3 +7,7 @@ class TeeRecipe(ConanFile):
 
     def requirements(self):
         self.requires("cmocka/1.1.5")
+
+    def layout(self):
+        cmake_layout(self)
+        
