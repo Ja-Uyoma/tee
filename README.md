cdb8fa65737689c5e1a73408e446473d32ec65f3
Jimmy Givans <jauyoma.dev@gmail.com>
2025-12-06 15:38:23 +0300

    refactor: use cmake-init project structure

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tee
- A Unix utility program that reads from stdin and writes to stdout and other files
+
+This is the tee project - a Unix utility program that reads from stdin and writes to stdout and other files
+
+# Building and installing
+
+See the [BUILDING](BUILDING.md) document.
+
+# Contributing
+
+See the [CONTRIBUTING](CONTRIBUTING.md) document.
+
+# Licensing
+
+This project is licensed under the MIT license. See [LICENSE] for more information.
--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,15 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.cmake import CMake
 
 class TeeRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+
+    def layout(self):
+        self.folders.generators = "conan"
+
+    def requirements(self):
+        self.requires("hedley/15")
 
     def build_requirements(self):
         self.test_requires("cmocka/1.1.7")
-
-    def layout(self):
-        cmake_layout(self)
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
